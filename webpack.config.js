84511de64de98744df0410bb8d09dd980dcb213a
pavel <pashkes@gmail.com>
2019-04-30 15:34:23 +0300

    added an exception for EsLint in the file config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,11 @@ module.exports = {
   entry: `./src/index.js`,
   output: {
     filename: `bundle.js`,
+    // eslint-disable-next-line no-undef
     path: path.join(__dirname, `public`)
   },
   devServer: {
+    // eslint-disable-next-line no-undef
     contentBase: path.join(__dirname, `public`),
     compress: false,
     port: 1337,
