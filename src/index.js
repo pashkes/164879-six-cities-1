54a873aff62c62cf4fb57873167328f67a7f0ef6
pavel <pashkes@gmail.com>
2019-05-03 14:49:47 +0300

    validated propTypes, created card of place

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import MainPage from "./components/main-page.jsx";
+import MainPage from "./components/main-page/main-page.jsx";
+
+const OFFERS = [`Beautiful & luxurious apartment at great location`, `Wood and stone place`, `Canal View Prinsengracht`, `Nice, cozy, warm big bed apartment`, `Wood and stone place`];
 
 const initApp = () => {
   ReactDOM.render(
-      <MainPage/>,
+      <MainPage
+        offers={OFFERS}
+      />,
       document.getElementById(`root`)
   );
 };
+
 initApp();
 
