7ecb081da04a66dd1be81fa19abd59a01f422b12
pavel <pashkes@gmail.com>
2019-05-03 15:59:03 +0300

    added text for favorite card and added symbol percent for rating

--- src/components/card-place/card-place.jsx
+++ src/components/card-place/card-place.jsx
@@ -19,12 +19,12 @@ const CardPlace = ({title, photoURL, isPremium, isFavorite, price, type, rating}
           <svg className="place-card__bookmark-icon" width="18" height="19">
             <use xlinkHref="#icon-bookmark"/>
           </svg>
-          <span className="visually-hidden">To bookmarks</span>
+          <span className="visually-hidden">{isFavorite ? `In` : `To`} bookmarks</span>
         </button>
       </div>
       <div className="place-card__rating rating">
         <div className="place-card__stars rating__stars">
-          <span style={{width: rating}}/>
+          <span style={{width: `${rating}%`}}/>
           <span className="visually-hidden">Rating</span>
         </div>
       </div>
