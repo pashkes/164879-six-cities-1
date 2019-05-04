f502c4e454ee99a5321f983225db6fd39037b4bd
pavel <pashkes@gmail.com>
2019-05-04 21:29:58 +0300

    added test for check states the card of place

--- src/components/card-place/card-place.test.js
+++ src/components/card-place/card-place.test.js
@@ -2,9 +2,25 @@ import React from 'react';
 import CardPlace from './card-place.jsx';
 import renderer from 'react-test-renderer';
 
-it(`renders is correctly`, () => {
-  const card = renderer
-    .create(<CardPlace/>)
-    .toJSON();
-  expect(card).toMatchSnapshot();
+describe(`renders correctly`, ()=> {
+  it(`in the default state`, () => {
+    const card = renderer
+      .create(<CardPlace/>)
+      .toJSON();
+    expect(card).toMatchSnapshot();
+  });
+
+  it(`if the card is favorite`, ()=> {
+    const cardIsFavorite = renderer
+      .create(<CardPlace isFavorite={true} />)
+      .toJSON();
+    expect(cardIsFavorite).toMatchSnapshot();
+  });
+
+  it(`if the card is premium`, ()=> {
+    const cardIsPremium = renderer
+      .create(<CardPlace isPremium={true} />)
+      .toJSON();
+    expect(cardIsPremium).toMatchSnapshot();
+  });
 });
