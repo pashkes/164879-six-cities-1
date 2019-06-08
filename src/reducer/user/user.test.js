d863ab1968633884e9ebde413448800b792c7bdc
pavel <pashkes@gmail.com>
2019-06-08 23:15:55 +0300

    added user test

--- src/reducer/user/user.test.js
+++ src/reducer/user/user.test.js
@@ -9,7 +9,7 @@ describe(`Reducer works correctly`, () => {
     const dispatch = jest.fn();
     const api = createApi(dispatch);
     const apiMock = new MockAdapter(api);
-    const authorization = Operation.authorization();
+    const authorization = Operation.authorization(``, ``);
 
     apiMock
       .onPost(`/login`, {params: {fake: true}})
@@ -17,8 +17,8 @@ describe(`Reducer works correctly`, () => {
 
     return authorization(dispatch, jest.fn(), api)
       .then(() => {
-        expect(dispatch).toHaveBeenCalledTimes(1);
-        expect(dispatch).toHaveBeenNthCalledWith(1, {
+        expect(dispatch).toHaveBeenCalledTimes(2);
+        expect(dispatch).toHaveBeenNthCalledWith(2, {
           type: ActionType.REQUIRED_AUTHORIZATION,
           payload: {params: {fake: true}},
         });
